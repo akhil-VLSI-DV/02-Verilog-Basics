ed515e4258570a32e463a784181a1d9fc44bb502
akhil <dm.akhilv@gmail.com>
2026-08-08 15:35:29 +0530

    Add testbench for AND gate functionality

---
+++ tb/and_gate_tb.v
@@ -0,0 +1,31 @@
+module and_gate_tb;
+
+reg a;
+reg b;
+wire y;
+
+and_gate dut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+
+    a = 0; b = 0;
+    #10;
+
+    a = 0; b = 1;
+    #10;
+
+    a = 1; b = 0;
+    #10;
+
+    a = 1; b = 1;
+    #10;
+
+    $finish;
+
+end
+
+endmodule
